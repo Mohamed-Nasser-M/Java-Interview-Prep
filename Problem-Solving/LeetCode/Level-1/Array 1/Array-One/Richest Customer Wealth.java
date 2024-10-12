2e1ef107183c3b9e5ebff678237c920e2e7f91d9
Mohamed Nasser M. <83620603+Mohamed-Nasser-M@users.noreply.github.com>
2024-10-12 20:55:01 +0300

    Update Richest Customer Wealth.java

--- Problem-Solving/LeetCode/Level-1/Array 1/Array-One/Richest Customer Wealth.java
+++ Problem-Solving/LeetCode/Level-1/Array 1/Array-One/Richest Customer Wealth.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxWealth = 0,currentWealth = 0;
+        for (int i = 0; i < accounts.length; i++) {
+            currentWealth=0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                currentWealth+=accounts[i][j];
+            }
+            maxWealth = Math.max(maxWealth,currentWealth);
+        }
+        return maxWealth;
+    }
+}
