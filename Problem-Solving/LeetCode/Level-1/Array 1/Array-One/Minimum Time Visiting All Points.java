bd0922ffa688133e6ac45885c6b32543a36e77c6
Mohamed Nasser M. <83620603+Mohamed-Nasser-M@users.noreply.github.com>
2024-10-14 22:14:08 +0300

    Create Minimum Time Visiting All Points.java

---
+++ Problem-Solving/LeetCode/Level-1/Array 1/Array-One/Minimum Time Visiting All Points.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int sum =0;
+        for (int i = 1; i < points.length; i++) {
+            sum += Math.max(Math.abs(points[i][0]-points[i-1][0]), Math.abs(points[i][1]-points[i-1][1]));
+        }
+        return sum;
+    }
+}
