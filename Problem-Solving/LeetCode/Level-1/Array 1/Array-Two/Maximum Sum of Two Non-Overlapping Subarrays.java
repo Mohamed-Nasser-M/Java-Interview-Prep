112d909b7fbebf106f0124a6c1c219cd1b008b9e
Mohamed Nasser M. <83620603+Mohamed-Nasser-M@users.noreply.github.com>
2024-10-16 16:20:15 +0300

    Update Maximum Sum of Two Non-Overlapping Subarrays.java

--- Problem-Solving/LeetCode/Level-1/Array 1/Array-Two/Maximum Sum of Two Non-Overlapping Subarrays.java
+++ Problem-Solving/LeetCode/Level-1/Array 1/Array-Two/Maximum Sum of Two Non-Overlapping Subarrays.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public int maxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen) {
+        return Math.max(maxSum(nums, firstLen, secondLen), maxSum(nums, secondLen, firstLen));
+    }
+    private int maxSum(int[] nums, int L, int M) {
+        int n = nums.length;
+        int[] prefixSum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        int maxL = 0, maxSum = 0;
+        for (int i = L + M; i <= n; i++) {
+            maxL = Math.max(maxL, prefixSum[i - M] - prefixSum[i - M - L]);
+            maxSum = Math.max(maxSum, maxL + prefixSum[i] - prefixSum[i - M]);
+        }
+        
+        return maxSum;
+    }
+}
