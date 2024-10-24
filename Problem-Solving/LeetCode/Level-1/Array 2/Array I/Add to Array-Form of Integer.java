14b6f40e22ac4fcbd5de8e8617cf8198949070dd
Mohamed Nasser M. <83620603+Mohamed-Nasser-M@users.noreply.github.com>
2024-10-24 18:42:21 +0300

    Create Add to Array-Form of Integer.java

---
+++ Problem-Solving/LeetCode/Level-1/Array 2/Array I/Add to Array-Form of Integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+          List<Integer> result = new ArrayList<>();
+        int i = num.length - 1;
+        while (i >= 0 || k > 0) {
+            if (i >= 0) {
+                k += num[i];
+                i--;
+            }
+            result.add(k % 10);
+            k /= 10; 
+        }
+        Collections.reverse(result);
+        return result;
+    }
+} 
