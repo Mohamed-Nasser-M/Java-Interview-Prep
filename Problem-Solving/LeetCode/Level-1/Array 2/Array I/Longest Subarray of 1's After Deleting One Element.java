ba134e8895c6e17257ca0054880b19a660598f24
Mohamed Nasser M. <83620603+Mohamed-Nasser-M@users.noreply.github.com>
2024-10-25 10:52:00 +0300

    Create Longest Subarray of 1's After Deleting One Element.java

---
+++ Problem-Solving/LeetCode/Level-1/Array 2/Array I/Longest Subarray of 1's After Deleting One Element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int left = 0;
+        int maxLen = 0;
+        int zeroCount = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+            while (zeroCount > 1) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left);
+        }
+
+        return maxLen;
+    }
+}
